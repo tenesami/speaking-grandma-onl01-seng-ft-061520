eb3bb49c4a0be29f52085c25893a2728cfd21462
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-06-15 22:54:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
   elsif phrase == phrase.upcase
-    return NO, NOT SINCE 1938!
+    return 'NO, NOT SINCE 1938!'
   end
 
 end
